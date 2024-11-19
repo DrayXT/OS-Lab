0ed4d9a0e4e9ec6676248799bec0a23cfaafb807
Andrew <titaniumv22@gmail.com>
2024-11-19 19:37:24 +0700

    Add files via upload

---
+++ OS-Lab1.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <mutex>
+
+using namespace std;
+
+class Monitor
+{
+    mutex mtx;
+    condition_variable cv;
+    bool event_ready = false;
+    int event_id = 0;
+
+public:
+    void producerEvent()
+    {
+        while (true)
+        {
+            this_thread::sleep_for(chrono::seconds(1)); // Задержка в 1 секунду
+            unique_lock<mutex> lock(mtx);
+            event_ready = true;
+            ++event_id;
+            cout << "Producer sent event " << event_id << endl;
+            cv.notify_one(); // Уведомление потребителя
+        }
+    }
+
+    void consumerEvent()
+    {
+        while (true)
+        {
+            unique_lock<mutex> lock(mtx);
+            cv.wait(lock, [this] { return event_ready; }); // Ожидание события
+            cout << "Consumer received event " << event_id << endl << endl;
+            event_ready = false;
+        }
+    }
+};
+
+void main()
+{
+    Monitor monitor;
+
+    thread producer(&Monitor::producerEvent, &monitor);
+    thread consumer(&Monitor::consumerEvent, &monitor);
+
+    // Ожидание завершения потоков
+    producer.join();
+    consumer.join();
+}
